14d8ee8b679bb8667268c9493637ab8481850d8c
phrudina <plgrudina@gmail.com>
2022-04-27 14:55:28 +0300

    Added struct

---
+++ src/main/java/com/company/dao/ClientDao.java
@@ -0,0 +1,8 @@
+package com.company.dao;
+
+public class ClientDao {
+
+//    write here all code related to client model
+//    and that code should implements CRUD operations
+//    repeat that for each dto classes
+}
---
+++ src/main/java/com/company/service/ClientService.java
@@ -0,0 +1,8 @@
+package com.company.service;
+
+import com.company.dao.ClientDao;
+
+public class ClientService {
+
+    private ClientDao clientDao = new ClientDao();
+}
