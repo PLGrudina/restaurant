5a3acc3641c41cad6e46ebdf0429cfe5da4f50e8
phrudina <plgrudina@gmail.com>
2022-06-02 15:59:57 +0300

    ClientDao example

--- src/main/java/com/company/dto/OrderBook.java
+++ src/main/java/com/company/dto/OrderBook.java
@@ -3,12 +3,14 @@ package com.company.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 import java.util.Map;
 
 @Getter
 @Setter
+@ToString
 @AllArgsConstructor
 
 public class OrderBook {
